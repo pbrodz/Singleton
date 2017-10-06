46a4951bbf3fbc48acc65b854c784d77b3b9d134
Paul Brodzinski <pbrodz@hotmail.com>
2017-10-06 14:58:08 -0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Singleton.Instance.Server);
+            Console.WriteLine(Singleton.Instance.Server);
+            Console.WriteLine(Singleton.Instance.Server);
+            Console.WriteLine(Singleton.Instance.Server);
+            Console.WriteLine(Singleton.Instance.Server);
+            Console.ReadLine();
+        }
+    }
+
+    public sealed class Singleton
+    {
+        private static readonly Lazy<Singleton> _instance = new Lazy<Singleton>(() => new Singleton());
+        private List<string> _servers = new List<string>();
+        private Singleton()
+        {
+            _servers.Add("Server1");
+            _servers.Add("Server2");
+            _servers.Add("Server3");
+            _servers.Add("Server4");
+            _servers.Add("Server5");
+        }
+        public static Singleton Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+        public string Server
+        {
+            get
+            {
+                int r = new Random().Next(_servers.Count);
+                return _servers[r].ToString();
+            }
+        }
+    }
+}
